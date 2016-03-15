d56f900ecd430f64cddd5fc8423d510d03f64643
Paul <paullehenaff@msn.com>
2016-03-15 14:37:33 +0000

    Added tween effect on start menu

--- MainMenu.js
+++ MainMenu.js
@@ -52,12 +52,16 @@ BasicGame.MainMenu.prototype = {
         //this.drawTube(graphics, this.tunnelPhysicsData);
         this.addForeground();
 
-	    this.title_image = this.add.sprite(this.game.width/2, this.game.height/2, 'title_image');
-	    this.title_image.anchor.set(0.5, 0.5);
+	    var title_image = this.add.sprite(this.game.width/2, this.game.height/2, 'title_image');
+	    title_image.anchor.set(0.5, 0.5);
+        title_image.alpha = 0.1;
+        this.add.tween(title_image).to( { alpha: 1 }, 500, "Linear", true );
 	    //this.title_image.scale.setTo(1, 1);
 
-        this.playButton = this.add.button(this.game.width/2, (this.game.height/2) + 200, 'start_button', this.startGame, this, 'over', 'out', 'down');
-        this.playButton.anchor.set(0.5, 0.5);
+        var playButton = this.add.button(this.game.width/2, (this.game.height/2) + 200, 'start_button', this.startGame, this, 'over', 'out', 'down');
+        playButton.anchor.set(0.5, 0.5);
+        playButton.alpha = 0.1;
+        this.add.tween(playButton).to( { alpha: 1 }, 500, "Linear", true );
 
 
 	},
@@ -83,22 +87,6 @@ BasicGame.MainMenu.prototype = {
 
 	},
 
-	resize: function (width, height) {
-
-		//	If the game container is resized this function will be called automatically.
-		//	You can use it to align sprites that should be fixed in place and other responsive display things.
-
-	    this.bg.width = width;
-	    this.bg.height = height;
-
-	    //this.title_image.anchor.set(0.5, 0.5);
-	    this.title_image.x = this.game.width/2;
-	    this.title_image.y = this.game.height/2;
-	    this.title_image.scale.setTo(0.5, 0.5);
-
-	    this.playButton.x = this.game.width/2;
-	    this.playButton.y = (this.game.height/2)+100;
-	},
 
 	startGame: function (pointer) {
 
