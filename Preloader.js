e83724dceac22196831accc468096dfea47aa590
Danny Victor <dvictor@Mines.edu>
2016-03-14 19:07:24 -0600

    Added mute button
    toggles all game sounds on or off

--- Preloader.js
+++ Preloader.js
@@ -39,6 +39,7 @@ BasicGame.Preloader.prototype = {
 
 		this.load.atlas('start_button', 'assets/GUI/start_button_atlas.png', 'assets/GUI/button_texture_atlas.json');
 		this.load.atlas('menu_button', 'assets/GUI/menu_button_atlas.png', 'assets/GUI/button_texture_atlas.json');
+		this.load.atlas('mute_button', 'assets/GUI/mute_button_atlas.png', 'assets/GUI/button_texture_atlas.json');
 
 
 		///////////
